7a31d23a8e49703f452f7f7a09386000ab01b335
Vasja Pavlovic <vasja.pavlovic@gmail.com>
2018-10-30 14:21:00 +0100

    prvi commit

---
+++ styles/tema.py
@@ -0,0 +1,22 @@
+from django import
+
+
+# asdasdasd
+
+asd
+'asdasda'
+
+# asdasd
+# asdasdasd
+# asdasdasdsa
+
+a = 1
+
+class Meta:
+
+if a > 1:
+    asdasd
+
+
+    color: @blue;
+    font-weight: bold;
